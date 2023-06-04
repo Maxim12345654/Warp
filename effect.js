aa88f3420f307b977ff76584edb9c3a488470bb2
maxim <maxikus01240323@gmail.com>
2023-06-04 22:12:05 +0700

    added class effect

--- effect.js
+++ effect.js
@@ -1,1 +1,14 @@
-1
+export class Effect{
+    constructor(canvas,ctx,image){
+        this.canvas = canvas;
+        this.ctx = ctx;
+        this.image = image;    
+        this.particleArray = [];    
+    }
+    init(){
+        const centerx = this.canvas.width / 2 - this.image.width / 2;
+        const centery = this.canvas.height / 2 - this.image.height / 2;
+        this.ctx.drawImage(this.image,centerx,centery);
+        const pixels = this.ctx.getImageData(0,0,this.canvas.width,this.canvas.height).data;
+    }
+}
--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+import { Effect } from "./effect.js";
 const image = new Image();
 image.src = "angler.png";
 image.addEventListener("load", function () {
@@ -5,8 +6,10 @@ image.addEventListener("load", function () {
     const ctx = canvas.getContext("2d");
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
+    const effect = new Effect(canvas,ctx,image);
+    effect.init();
     function animate(){
-    canvas.clearRect(0,0,canvas.width,canvas.height);
+    ///canvas.clearRect(0,0,canvas.width,canvas.height);
     requestAnimationFrame(animate);
     }
     animate();
